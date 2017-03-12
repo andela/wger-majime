e148367becbedf59360db90260b27f5972349fa4
Bolaji Olajide <bolaji.olajide@andela.com>
2017-03-12 07:20:07 +0100

    [feature #139473445] Add the language field and set the language instance

--- wger/exercises/views/exercises.py
+++ wger/exercises/views/exercises.py
@@ -187,7 +187,8 @@ class ExercisesEditAddView(WgerFormMixin):
                           'muscles_secondary',
                           'equipment',
                           'license',
-                          'license_author']
+                          'license_author',
+                          'language']
 
             class Media:
                 js = ('/static/bower_components/tinymce/tinymce.min.js',)
@@ -223,7 +224,7 @@ class ExerciseAddView(ExercisesEditAddView, LoginRequiredMixin, CreateView):
         '''
         Set language, author and status
         '''
-        form.instance.language = load_language()
+        form.instance.language = load_language(form.instance.language.short_name)
         form.instance.set_author(self.request)
         return super(ExerciseAddView, self).form_valid(form)
 
